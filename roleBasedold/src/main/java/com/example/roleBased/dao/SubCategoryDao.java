51c4874eb602b1b68604ebe7b0d068b27b0b6199
Nancy Gupta <nancy_gupta@persistent.com>
2023-07-31 11:52:19 +0530

    no change

--- roleBasedold/src/main/java/com/example/roleBased/dao/SubCategoryDao.java
+++
@@ -1,9 +0,0 @@
-package com.example.roleBased.dao;
-
-import com.example.roleBased.entity.SubCategory;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SubCategoryDao extends JpaRepository<SubCategory,Integer> {
-}
--- roleBasedold/src/main/java/com/example/roleBased/entity/Coupon.java
+++
@@ -1,4 +0,0 @@
-package com.example.roleBased.entity;
-
-public class Coupon {
-}
